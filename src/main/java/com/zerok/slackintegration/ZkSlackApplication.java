3072599980802f42862ee1b034fa574e69c4444c
CHNG7 <nikhilchinta7@gmail.com>
2023-10-13 13:30:45 +0530

    main class bug fix

--- src/main/java/com/zerok/slackintegration/ZkSlackApplication.java
+++ src/main/java/com/zerok/slackintegration/ZkSlackApplication.java
@@ -3,13 +3,13 @@ package com.zerok.slackintegration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-gs
+
 @SpringBootApplication
 @ServletComponentScan
 public class ZkSlackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZkSlackApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZkSlackApplication.class, args);
+    }
 
 }
