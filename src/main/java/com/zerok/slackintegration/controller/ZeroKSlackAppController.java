740c351422da58a40804f67e58042946ee94a606
CHNG7 <nikhilchinta7@gmail.com>
2023-10-18 10:25:46 +0530

    redirection URL

--- src/main/java/com/zerok/slackintegration/controller/ZeroKSlackAppController.java
+++ src/main/java/com/zerok/slackintegration/controller/ZeroKSlackAppController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.IOException;
 import java.net.URI;
@@ -35,10 +36,10 @@ public class ZeroKSlackAppController {
     }
 
     @GetMapping(value = "/initiate")
-    public ResponseEntity<Void> testredirect(@RequestParam(name = "userId") String userId, @RequestParam(name = "orgId") String org) {
+    public RedirectView testredirect(@RequestHeader(value = "X-USER-ID", required = true) String userId, @RequestHeader(value = "X-ORG-ID", required = true)  String org) {
         URI slackOAuthRedirectionUri = slackOAuthService.createSlackOAuthRedirectionUri(userId,org);
         System.out.println("redirection URL : "+ slackOAuthRedirectionUri.toString());
-        return ResponseEntity.status(HttpStatus.FOUND).location(slackOAuthRedirectionUri).build();
+        return new RedirectView(slackOAuthRedirectionUri.toString());
     }
 
     // get slack integration for user id and org id
